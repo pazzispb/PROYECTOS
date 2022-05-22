c9b036121f7e531f37332a381b29c4e8f932f2e1
PAZZIS ALEJANDRO PAULINO BATISTA <1103790@est.intec.edu.do>
2022-05-21 23:52:36 -0400

    Clase ciudadano creada

---
+++ CRUD_CEDULA/Cedula.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace CRUD_CEDULA
+{
+    partial class Cedula
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Cedula
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(838, 422);
+            this.Name = "Cedula";
+            this.Text = "Registro de Ciudadano";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
